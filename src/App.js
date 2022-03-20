de54ff42ebe8be5ff2bff17f60ed2ddee342bab4
Crawford Vining <crawpdx@gmail.com>
2022-03-20 10:56:18 -0700

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  const urls = {
+    people: "https://swapi.dev/api/people/?format=json",
+    planets: "https://swapi.dev/api/planets/?format=json",
+    starships: "https://swapi.dev/api/starships/?format=json",
+    vehicles: "https://swapi.dev/api/vehicles/?format=json",
+    species: "https://swapi.dev/api/species/?format=json",
+    films: "https://swapi.dev/api/films/?format=json"
+  }
+
+const [results, setResults] = useState({})
+
+const fetchApis = async () => {
+    try{
+        const responses = await Promise.all(Object.entries(urls).map(async ([ key, url]) => {
+            const res = await fetch(url)
+            return [ key, (await res.json()).results ]
+        }))
+
+        return Object.fromEntries(responses)
+    } catch (err){
+        console.log(err)
+    }
+}
+
+useEffect(() => {
+    fetchApis().then(setResults)
+    
+}, [])
+
+console.log(results)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SW URLs</h1>
+      
     </div>
   );
 }
